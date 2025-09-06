7540cc323ae6cbb3dbe859abc6603719686672a0
maxfbal-prog <maxfbal@gmail.com>
2025-09-06 16:07:22 -0400

    Add files via upload

---
+++ docs/scripts/physicsProjectileApp1.js
@@ -0,0 +1,10 @@
+// JavaScript source code
+function display()
+{
+    var gallons=document.getElementById("gallons").value;
+    
+
+    document.getElementById("gallonsDisplay").innerHTML = gallons;
+
+
+}
